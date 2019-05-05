e40895c44903b4e7674394b0a36a8e8de85b896a
Aron <455146062@qq.com>
2019-05-06 00:27:12 +0800

    缓冲界面后就奔溃了，不知道哪里错了

---
+++ app/src/main/java/com/group/friendfinder/Base/BaseSub.java
@@ -0,0 +1,13 @@
+package com.group.friendfinder.Base;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.group.friendfinder.R;
+
+public class BaseSub extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);}
+}
