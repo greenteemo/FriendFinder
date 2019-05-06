84b5a1c8163fddc28110e15197ce96e9c9c4eb8c
greenteemo <greenteemo1013@gmail.com>
2019-05-06 13:14:33 +0800

    done: log into server

--- app/src/main/java/com/group/friendfinder/View/RestClient.java
+++ app/src/main/java/com/group/friendfinder/View/RestClient.java
@@ -9,41 +9,43 @@ import java.util.Scanner;
 public class RestClient {
     private static final String BASE_URI = "http://z2462w1583.qicp.vip:29989/MonashFriendFinder/webresources";
 
-//    /*
-//    Task1 Invoking public web APIs
-//    (c) get movie
-//     */
-//    public static String getMovie(String movieName) {
-//        //initialise
-//        URL url = null;
-//        HttpURLConnection conn = null;
-//        String textResult = "";
-//        //Making HTTP request
-//        try {
-//            url = new URL("https://www.googleapis.com/customsearch/v1?key=AIzaSyAMoB0LYGyXSFcuNxISWHEOJGdMTQb-HVk&cx=016750410111980342498:qkhodgxfizq&q=" + movieName + "&num=1");
-//            //open the connection
-//            conn = (HttpURLConnection) url.openConnection();
-//            //set the timeout
-//            conn.setReadTimeout(10000);
-//            conn.setConnectTimeout(15000);
-//            //set the connection method to GET
-//            conn.setRequestMethod("GET");
-//            //add http headers to set your response type to json
-//            conn.setRequestProperty("Content-Type", "application/json");
-//            conn.setRequestProperty("Accept", "application/json");
-//            //Read the response
-//            Scanner inStream = new Scanner(conn.getInputStream());
-//            //read the input steream and store it as string
-//            while (inStream.hasNextLine()) {
-//                textResult += inStream.nextLine();
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        } finally {
-//            conn.disconnect();
-//        }
-//        return textResult;
-//    }
+    /*
+    Task3 Login
+    (a) get student
+     */
+    public static String login(String smonashEmail, String spassword) {
+        final String methodPath = "/monashfriendfinder.mffprofile/login/" + smonashEmail + "/" + spassword;
+        //initialise
+        URL url = null;
+        HttpURLConnection conn = null;
+        String textResult = "";
+        //Making HTTP request
+        try {
+            url = new URL(BASE_URI + methodPath);
+            //open the connection
+            conn = (HttpURLConnection) url.openConnection();
+            //set the timeout
+            conn.setReadTimeout(10000);
+            conn.setConnectTimeout(15000);
+            //set the connection method to GET
+            conn.setRequestMethod("GET");
+            //add http headers to set your response type to json
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Accept", "application/json");
+            //Read the response
+            Scanner inStream = new Scanner(conn.getInputStream());
+            //read the input steream and store it as string
+            while (inStream.hasNextLine()) {
+                textResult += inStream.nextLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            conn.disconnect();
+        }
+        return textResult;
+    }
+
 
     /*
     Task4 Editing Records and Search Screen
