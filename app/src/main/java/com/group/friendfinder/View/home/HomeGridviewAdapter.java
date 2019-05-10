cc89a5cdfae31ab31498b9ba6b8802c12b0cb32c
JenetCSDN <15871387642@163.com>
2019-05-10 21:50:39 +0800

    减少func

--- app/src/main/java/com/group/friendfinder/View/home/HomeGridviewAdapter.java
+++ app/src/main/java/com/group/friendfinder/View/home/HomeGridviewAdapter.java
@@ -14,8 +14,10 @@ public class HomeGridviewAdapter extends BaseAdapter{
 
     private Context mContext;
     private LayoutInflater mLayoutInflater;
-    int[] icon = {R.mipmap.ic_launcher, R.mipmap.home, R.mipmap.search, R.mipmap.movie, R.mipmap.pie, R.mipmap.bar};
-    String[] iconName = {"exampleActivity","faction1", "faction2", "movie temp", "Favorite Units", "Location BarChart"};
+//    int[] icon = {R.mipmap.ic_launcher, R.mipmap.home, R.mipmap.search, R.mipmap.movie, R.mipmap.pie, R.mipmap.bar};
+//    String[] iconName = {"exampleActivity","faction1", "faction2", "movie temp", "Favorite Units", "Location BarChart"};
+    int[] icon = {R.mipmap.movie, R.mipmap.pie, R.mipmap.bar};
+    String[] iconName = {"movie temp", "Favorite Units", "Location BarChart"};
 
     public HomeGridviewAdapter(Context context)
     {
